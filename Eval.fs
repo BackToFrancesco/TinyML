59cefd543789c5cf32502283932bacc301ac8947
BackToFrancesco <bacchin.francesco2000@gmail.com>
2023-02-15 11:45:59 +0100

    added evaluation for let-rec and for operators

--- Eval.fs
+++ Eval.fs
@@ -45,28 +45,110 @@ let rec eval_expr (env : value env) (e : expr) : value =
         let v1 = eval_expr env e1
         eval_expr ((x, v1) :: env) e2
 
-    // TODO: test this is ok or fix it
     | LetRec (f, _, e1, e2) -> 
         let v1 = eval_expr env e1
-        match v1 with
-        | Closure (venv1, x, e) -> RecClosure (venv1, f, x, e)
-        | _ -> unexpected_error "eval_expr: expected closure in rec binding but got: %s" (pretty_value v1)
-        // TODO finish this implementation
+        let vc = 
+            match v1 with
+            | Closure (venv1, x, e) -> RecClosure (venv1, f, x, e)
+            | _ -> unexpected_error "eval_expr: expected closure in rec binding but got: %s" (pretty_value v1)
 
-    | BinOp (e1, "+", e2) -> binop (+) (+) env e1 e2
-    | BinOp (e1, "-", e2) -> binop (-) (-) env e1 e2
-    | BinOp (e1, "*", e2) -> binop ( * ) ( * ) env e1 e2
-    // TODO: implement other binary ops
-    | Tuple es -> VTuple (List.map (eval_expr env) es) // TODO: ricontrollare
+        eval_expr ((f, vc)::env) e2
+
+    // Binary arithmetic op for int
+    | BinOp (e1, "+", e2) -> binop_arithmetic_int (+) env e1 e2
+    | BinOp (e1, "-", e2) -> binop_arithmetic_int (-) env e1 e2
+    | BinOp (e1, "*", e2) -> binop_arithmetic_int ( * ) env e1 e2
+    | BinOp (e1, "/", e2) -> binop_arithmetic_int (/) env e1 e2
+    | BinOp (e1, "%", e2) -> binop_arithmetic_int (%) env e1 e2
+
+    // Binary arithmetic op for float
+    | BinOp (e1, "+.", e2) -> binop_arithmetic_float (+) env e1 e2
+    | BinOp (e1, "-.", e2) -> binop_arithmetic_float (-) env e1 e2
+    | BinOp (e1, "*.", e2) -> binop_arithmetic_float ( * ) env e1 e2
+    | BinOp (e1, "/.", e2) -> binop_arithmetic_float (/) env e1 e2
+
+    // Binary comparison op for int
+    | BinOp (e1, "<", e2) -> binop_comparison_int (<) env e1 e2
+    | BinOp (e1, "<=", e2) -> binop_comparison_int (<=) env e1 e2
+    | BinOp (e1, ">", e2) -> binop_comparison_int (>) env e1 e2
+    | BinOp (e1, ">=", e2) -> binop_comparison_int (>=) env e1 e2
+    | BinOp (e1, "=", e2) -> binop_comparison_int (=) env e1 e2
+    | BinOp (e1, "<>", e2) -> binop_comparison_int (<>) env e1 e2
+
+    // Binary comparison op for float
+    | BinOp (e1, "<.", e2) -> binop_comparison_float (<) env e1 e2
+    | BinOp (e1, "<=.", e2) -> binop_comparison_float (<=) env e1 e2
+    | BinOp (e1, ">.", e2) -> binop_comparison_float (>) env e1 e2
+    | BinOp (e1, ">=.", e2) -> binop_comparison_float (>=) env e1 e2
+    | BinOp (e1, "=.", e2) -> binop_comparison_float (=) env e1 e2
+    | BinOp (e1, "<>.", e2) -> binop_comparison_float (<>) env e1 e2
+
+    // AND OR op for bool
+    | BinOp (e1, "and", e2) -> binop_comparison_bool (&&) env e1 e2
+    | BinOp (e1, "or", e2) -> binop_comparison_bool (||)  env e1 e2
+
+    // NOT op for bool
+    | UnOp ("not", e) -> unop_not (not) env e
+
+    // neg op for int => neg x = -x 
+    | UnOp ("neg", e) -> unop_neg_int (~-) env e
+
+    //neg op for float 
+    | UnOp ("neg.", e) -> unop_neg_float (~-) env e
+
+    | Tuple es -> VTuple (List.map (eval_expr env) es)
 
     | _ -> unexpected_error "eval_expr: unsupported expression: %s [AST: %A]" (pretty_expr e) e
 
-and binop op_int op_float env e1 e2 =
+and binop_arithmetic_int op env e1 e2 =
     let v1 = eval_expr env e1
     let v2 = eval_expr env e2
     match v1, v2 with
-    | VLit (LInt x), VLit (LInt y) -> VLit (LInt (op_int x y))
-    | VLit (LFloat x), VLit (LFloat y) -> VLit (LFloat (op_float x y))
-    | VLit (LInt x), VLit (LFloat y) -> VLit (LFloat (op_float (float x) y))
-    | VLit (LFloat x), VLit (LInt y) -> VLit (LFloat (op_float x (float y)))
-    | _ -> unexpected_error "eval_expr: illegal operands in binary operator (+): %s + %s" (pretty_value v1) (pretty_value v2)
+    | VLit (LInt x), VLit (LInt y) -> VLit (LInt (op x y))
+    | _ -> unexpected_error "eval_expr: illegal operands in binary int arithmetic operator: %s, %s" (pretty_value v1) (pretty_value v2)
+
+and binop_arithmetic_float op env e1 e2 =
+    let v1 = eval_expr env e1
+    let v2 = eval_expr env e2
+    match v1, v2 with
+    | VLit (LFloat x), VLit (LFloat y) -> VLit (LFloat (op x y))
+    | _ -> unexpected_error "eval_expr: illegal operands in binary float arithmetic operator: %s, %s" (pretty_value v1) (pretty_value v2)
+
+and binop_comparison_int op env e1 e2 =
+    let v1 = eval_expr env e1
+    let v2 = eval_expr env e2
+    match v1, v2 with
+    | VLit (LInt x), VLit (LInt y) -> VLit (LBool (op x y))
+    | _ -> unexpected_error "eval_expr: illegal operands in binary int comparison operator: %s, %s" (pretty_value v1) (pretty_value v2)
+
+and binop_comparison_float op env e1 e2 =
+    let v1 = eval_expr env e1
+    let v2 = eval_expr env e2
+    match v1, v2 with
+    | VLit (LFloat x), VLit (LFloat y) -> VLit (LBool (op x y))
+    | _ -> unexpected_error "eval_expr: illegal operands in binary float comparison operator: %s, %s" (pretty_value v1) (pretty_value v2)
+
+and binop_comparison_bool op env e1 e2 =
+    let v1 = eval_expr env e1
+    let v2 = eval_expr env e2
+    match v1, v2 with
+    | VLit (LBool x), VLit (LBool y) -> VLit (LBool (op x y))
+    | _ -> unexpected_error "eval_expr: illegal operands in binary boolean comparison operator: %s, %s" (pretty_value v1) (pretty_value v2)
+
+and unop_not op env e =
+    let v = eval_expr env e
+    match v with
+    | VLit (LBool x) -> VLit (LBool (op x))
+    | _ -> unexpected_error "eval_expr: illegal operands in unary not operator: %s" (pretty_value v)
+
+and unop_neg_int op env e =
+    let v = eval_expr env e
+    match v with
+    | VLit (LInt x) -> VLit (LInt (op x))
+    | _ -> unexpected_error "eval_expr: illegal operands in unary int neg operator: %s" (pretty_value v)
+
+and unop_neg_float op env e =
+    let v = eval_expr env e
+    match v with
+    | VLit (LFloat x) -> VLit (LFloat (op x))
+    | _ -> unexpected_error "eval_expr: illegal operands in unary float neg operator: %s" (pretty_value v)
